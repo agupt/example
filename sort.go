94cc3a134262d20d9221357e6e9e1dd128b959ad
Amool <amool.gupta@salesforce.com>
2015-08-03 16:46:54 -0700

    practicing bubble sort in go

---
+++ sort.go
@@ -0,0 +1,25 @@
+package main 
+
+import "fmt"
+
+func insertionSort(input []int) int {
+	fmt.Printf("sorting %v of length %v\n", input, len(input))
+	var loop int = 0
+	for i := 0 ; i < len(input); i++ {
+		for j := i ; j < len(input) ; j++ {
+			loop = loop + 1
+			if input[j] < input [i] {
+				temp := input[i]
+				input[i] = input[j]
+				input[j] = temp
+			}
+		}
+	}
+	return loop
+}
+
+func main() {
+	input := []int{5,4,3,2,1}
+	loop := insertionSort(input)
+	fmt.Printf("looped %v times %v\n", loop, input)
+}
