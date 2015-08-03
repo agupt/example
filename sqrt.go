02941eb6ed7c94e4d352887fde94a69601411f3c
Amool <amool.gupta@salesforce.com>
2015-08-03 16:20:26 -0700

    add functionality to scan input from command line

--- sqrt.go
+++ sqrt.go
@@ -1,3 +1,4 @@
+// file to simple square root calculation, start with half of number while calculating square root using newton's formula
 package main
 
 import "fmt"
@@ -5,27 +6,27 @@ import "fmt"
 const Delta = 0.001
 
 func nextSqrt(cur float64, x int) float64 {
-	return (cur - ((cur * cur) - float64(x)) / (2 * cur))
+	return (cur - ((cur*cur)-float64(x))/(2*cur))
 }
 
 func sqrtWithTries(x int, start float64, tries int) float64 {
 	var next float64 = float64(start)
-	for i := 0 ; i < tries ; i++ {
+	for i := 0; i < tries; i++ {
 		next = nextSqrt(next, x)
 	}
 	return next
 }
 
 func chooseStart(x int) float64 {
-	return float64(x)/2
+	return float64(x) / 2
 }
 
 func sqrt(x int) float64 {
-	var cur float64 = sqrtWithTries(x,chooseStart(x), 5) // do 5 minimum tries
+	var cur float64 = sqrtWithTries(x, chooseStart(x), 5) // do 5 minimum tries
 	var next float64 = nextSqrt(cur, x)
 	for {
-		fmt.Printf(".")
-		if next - cur  < Delta {
+		fmt.Printf(".\n")
+		if next-cur < Delta {
 			return next
 		}
 		cur = next
@@ -35,9 +36,14 @@ func sqrt(x int) float64 {
 }
 
 func main() {
-	sqrtWithTries(10,3,10)
-	fmt.Printf("%v",sqrt(10))
-	//sqrt(10)
+	var val int
+	args, err := fmt.Scanln(&val)
+	if(err != nil) {
+		fmt.Printf("%v\n",err)
+	} else {
+		fmt.Printf("read %v args\n", args)
+	}
+	fmt.Printf("Calculating Square Root of %v\n", val)
+	fmt.Printf("sqrt of %v = %v\n", val, sqrt(val))
+	
 }
-
-
